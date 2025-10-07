e346567b1ba90b233d5a99724246a84ce991bf7a
williamsk3vin <williamsk3vin@gmail.com>
2025-10-07 13:26:49 -0700

    Update Reverse_String.py
    I have made 2 separate solutions, one with slicing and the other without

--- Reverse_String.py
+++ Reverse_String.py
@@ -1,6 +1,21 @@
+#with slicing
 class Solution:
     def reverseString(self, s: List[str]) -> None:
         """
         Do not return anything, modify s in-place instead.
         """
         s[::-1] = s
+
+#without slicing
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        left = 0
+        right = len(s) - 1
+        
+        while left < right:
+            s[left], s[right] = s[right], s[left]
+            left +=1 
+            right -= 1
