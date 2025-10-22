b3da3b6b1f0bea72efff2b8ff67cfd860a8d6cfb
williamsk3vin <williamsk3vin@gmail.com>
2025-10-22 15:21:21 -0700

    Update Customer_Placing_the_Largest_Number_of_Orders.py
    Pandas Exercise

--- Pandas/Data Aggregration/Customer_Placing_the_Largest_Number_of_Orders.py
+++ Pandas/Data Aggregration/Customer_Placing_the_Largest_Number_of_Orders.py
@@ -1,1 +1,7 @@
+import pandas as pd
+
+def largest_orders(orders: pd.DataFrame) -> pd.DataFrame:
+    order = orders.value_counts('customer_number').reset_index()
+    return order[['customer_number']].iloc[[0]]
+
 
