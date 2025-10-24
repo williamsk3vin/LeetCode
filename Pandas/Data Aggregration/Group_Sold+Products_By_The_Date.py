fd2841f2a616383b0366ded91e7c35175df7576a
williamsk3vin <williamsk3vin@gmail.com>
2025-10-23 22:07:53 -0600

    Create Group_Sold+Products_By_The_Date.py
    Pandas Exercise

---
+++ Pandas/Data Aggregration/Group_Sold+Products_By_The_Date.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
+    return activities.groupby('sell_date').agg(num_sold=('product', 'nunique'),products=('product', lambda x: ','.join(sorted(set(x))))).reset_index()
+
