2bc8132a69c0311b8132e9931ad2f7513edf8859
williamsk3vin <williamsk3vin@gmail.com>
2025-11-21 00:39:14 -0800

    Create Rank_Scores.py

---
+++ Pandas/Data Manipulation/Rank_Scores.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
+        scores["rank"] = scores["score"].rank(
+            method="dense",
+            ascending=False
+        ).astype(int)
+        return scores[['score','rank']].sort_values("score", ascending=False)
