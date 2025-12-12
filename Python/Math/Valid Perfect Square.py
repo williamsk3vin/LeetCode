97e58be255f87619a681fdf4d0dca6c3449616f1
williamsk3vin <williamsk3vin@gmail.com>
2025-12-11 21:39:59 -0800

    Create Valid Perfect Square.py

---
+++ Python/Math/Valid Perfect Square.py
@@ -0,0 +1,17 @@
+# O(log n)
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left = 1
+        right = num // 2
+        if num == 1:
+            return True
+        while left <= right:
+            mid = (left + right) // 2
+            if mid * mid == num:
+                return True
+            elif mid * mid > num:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return False
+
