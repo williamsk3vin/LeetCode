cfcf2407d1141f70173f11aa1c7aba18947fcae4
williamsk3vin <williamsk3vin@gmail.com>
2025-12-16 03:25:43 -0800

    Create Combine Two Tables.py

---
+++ Pandas/Database/Combine Two Tables.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def combine_two_tables(person: pd.DataFrame, address: pd.DataFrame) -> pd.DataFrame:
+    result = pd.merge(person, address, on='personId', how='left')
+    return result[['firstName','lastName','city','state']]
