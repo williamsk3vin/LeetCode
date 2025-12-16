0b846419e7d5e3809b62793fdc4ab4fb71fce5aa
williamsk3vin <williamsk3vin@gmail.com>
2025-12-15 23:42:00 -0800

    Update Add Digits.py

--- Python/Math/Add Digits.py
+++ Python/Math/Add Digits.py
@@ -1,3 +1,4 @@
+#Without Loops
 class Solution:
     def addDigits(self, num: int) -> int:
         if num == 0:
@@ -6,4 +7,18 @@ class Solution:
             return 1 + (num - 1) % 9
 
 
-
+#With Loops
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        
+        total = 0
+        while num:
+            total += num % 10
+            if num // 10 == 0 and total >= 10:
+                num = total
+                total = 0
+            else:
+                num //= 10
+        return total
