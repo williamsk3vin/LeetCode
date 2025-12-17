eec8c0cd4b9f3ca194ab463909f13b778b857710
williamsk3vin <williamsk3vin@gmail.com>
2025-12-17 00:20:34 -0800

    Create Ugly Number.py

---
+++ Python/Math/Ugly Number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n == 1:
+            return True
+        if n <= 0:
+            return False
+
+        while n > 1:
+            if n % 2 == 0:
+                n /= 2
+            elif n % 3 == 0:
+                n /= 3
+            elif n % 5 == 0:
+                n /= 5
+            else:
+                return False
+        return True
