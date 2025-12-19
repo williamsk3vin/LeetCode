161603cc9fd5ac7a1540faa5e1daf6e0d57cb14a
williamsk3vin <williamsk3vin@gmail.com>
2025-12-19 04:44:13 -0800

    Update Power of Four.py

--- Python/Math/Power of Four.py
+++ Python/Math/Power of Four.py
@@ -17,3 +17,7 @@ class Solution:
                 low = mid + 1
         return False
             
+#An interesting Solution
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        return n > 0 and (n & (n - 1)) == 0 and (n & 0x55555555) != 0
