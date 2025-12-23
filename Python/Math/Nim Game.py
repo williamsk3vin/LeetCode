3a28ec34a66247e8d61ed0cfab4f66bd1ba2afd8
williamsk3vin <williamsk3vin@gmail.com>
2025-12-23 00:07:46 -0800

    Create Nim Game.py

---
+++ Python/Math/Nim Game.py
@@ -0,0 +1,16 @@
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        if n % 4 == 0:
+            return False
+        else:
+            return True
+
+class Solution:
+    def canWinNim(self, n: int) -> bool:
+        if n % 4 == 0:
+            return False
+        return True
+
+  class Solution:
+    def canWinNim(self, n: int) -> bool:
+        return False if n % 4 == 0 else True
