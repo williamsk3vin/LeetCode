d48b582e242d8c12a27c5ed885a72590e6fd757b
williamsk3vin <williamsk3vin@gmail.com>
2025-12-30 00:07:44 -0800

    Update Top Travellers.py

--- Pandas/Database/Top Travellers.py
+++ Pandas/Database/Top Travellers.py
@@ -2,6 +2,5 @@ import pandas as pd
 
 def top_travellers(users: pd.DataFrame, rides: pd.DataFrame) -> pd.DataFrame:
     rides1 = rides.groupby('user_id')['distance'].sum().reset_index(name='travelled_distance')
-    merged = users.merge(rides1, left_on='id', right_on='user_id', how='left')
-    merged['travelled_distance'] = merged['travelled_distance'].fillna(0)
+    merged = users.merge(rides1, left_on='id', right_on='user_id', how='left').fillna(0)
     return merged[['name','travelled_distance']].sort_values(['travelled_distance','name'], ascending=[False, True])
