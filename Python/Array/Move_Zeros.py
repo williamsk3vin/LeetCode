46ed32d0da207968637db94e1945477d45ba1e38
williamsk3vin <williamsk3vin@gmail.com>
2026-07-10 14:27:28 -0700

    Update Move_Zeros.py

--- Python/Array/Move_Zeros.py
+++ Python/Array/Move_Zeros.py
@@ -9,3 +9,15 @@ class Solution:
             if nums[i] == 0:
                 nums.remove(nums[i])
                 nums.append(0)
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        write = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[write], nums[i] = nums[i], nums[write]
+                write += 1
