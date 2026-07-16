a3ad4901c54021182d0cd388686650d74c2dc078
williamsk3vin <williamsk3vin@gmail.com>
2026-07-16 13:28:57 -0700

    Update Valid_Parentheses.py

--- Python/Others/Valid_Parentheses.py
+++ Python/Others/Valid_Parentheses.py
@@ -18,3 +18,31 @@ class Solution:
                 return False
         
         return not stack
+
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        if len(s) % 2 != 0:
+            return False
+
+        for i in s:
+            if i in '({[':
+                stack.append(i)
+            else:
+                if not stack:
+                    return False
+
+                if i == ')' and stack[-1] == '(':
+                    stack.pop()
+                elif i == '}' and stack[-1] == '{':
+                    stack.pop()
+                elif i == ']' and stack[-1] == '[':
+                    stack.pop()
+                else:
+                    return False
+        if stack:
+            return False
+        else:
+            return True
