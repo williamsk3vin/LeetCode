72720fcf790c01132d96414c340e902ee2589763
williamsk3vin <williamsk3vin@gmail.com>
2026-07-17 17:54:25 -0700

    Create Next_Greater_Element_1.py

---
+++ Python/Stack/Next_Greater_Element_1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        greaterElement = [-1] * len(nums1)
+        maps = {}
+
+        for nums in nums2:
+            while stack and nums > stack[-1]:
+                value = stack.pop()
+                maps[value] = nums
+            stack.append(nums)
+        
+        for idx , nums in enumerate(nums1):
+            if nums in maps.keys():
+                greaterElement[idx] = maps[nums]
+
+        return greaterElement       
