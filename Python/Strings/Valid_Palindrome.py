7d6e743956f48f0479a605a08057b05ebeb53841
williamsk3vin <williamsk3vin@gmail.com>
2026-07-21 15:05:30 -0700

    Update Valid_Palindrome.py

--- Python/Strings/Valid_Palindrome.py
+++ Python/Strings/Valid_Palindrome.py
@@ -18,3 +18,26 @@ class Solution:
             else:
                 return False
         return True
+
+#Two Pointer O(1) space
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+                continue
+            
+            if not s[right].isalnum():
+                right -= 1
+                continue
+            
+            if s[left].lower() != s[right].lower():
+                return False
+
+            left += 1
+            right -= 1
+                
+        return True
