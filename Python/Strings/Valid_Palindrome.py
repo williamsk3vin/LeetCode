27cd5bd81c15b0fac72a642bf3b6a377c01c5110
williamsk3vin <williamsk3vin@gmail.com>
2026-07-21 15:00:38 -0700

    Update Valid_Palindrome.py

--- Python/Strings/Valid_Palindrome.py
+++ Python/Strings/Valid_Palindrome.py
@@ -3,3 +3,18 @@ class Solution:
         cleaned = "".join([char.lower() for char in s if char.isalnum()])
 
         return cleaned == cleaned[::-1]
+
+
+#Two Pointer
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s2 = ''.join(filter(str.isalnum, s)).lower()
+        left = 0
+        right = len(s2) - 1
+        while left <= right:
+            if s2[left] == s2[right]:
+                left += 1
+                right -= 1
+            else:
+                return False
+        return True
