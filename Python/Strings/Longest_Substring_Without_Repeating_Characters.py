5441811649943dd816477e6330d4b973de91825b
williamsk3vin <williamsk3vin@gmail.com>
2026-07-22 16:57:23 -0700

    Update Longest_Substring_Without_Repeating_Characters.py

--- Python/Strings/Longest_Substring_Without_Repeating_Characters.py
+++ Python/Strings/Longest_Substring_Without_Repeating_Characters.py
@@ -18,3 +18,18 @@ class Solution:
             substring_len = max(substring_len, i - start + 1)
         return substring_len
             
+
+#Window
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        left = 0
+        max_length = 0
+        seen = set()
+        for right in range(len(s)):
+            while s[right] in seen:
+                seen.remove(s[left])
+                left += 1
+            seen.add(s[right])
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
