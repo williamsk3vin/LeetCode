1f374a5f4071857801d61def167a35c40b68f83c
williamsk3vin <williamsk3vin@gmail.com>
2026-07-22 16:07:16 -0700

    Create Find the Highest Altitude.py

---
+++ Python/Array/Find the Highest Altitude.py
@@ -0,0 +1,8 @@
+class Solution:
+    def largestAltitude(self, gain: List[int]) -> int:
+        altitude = 0
+        highest = 0
+        for g in gain:
+            altitude += g
+            highest = max(altitude, highest)
+        return highest 
