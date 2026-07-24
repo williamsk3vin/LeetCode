61fac721860c14a0ae6bd30becf3fbe5aaa241b1
williamsk3vin <williamsk3vin@gmail.com>
2026-07-24 00:52:03 -0700

    Update Valid_Anagram.py

--- Python/Strings/Valid_Anagram.py
+++ Python/Strings/Valid_Anagram.py
@@ -34,3 +34,21 @@ class Solution:
             if value != 0:
                 return False
         return True
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        count = {}
+
+        for ch in s:
+            count[ch] = count.get(ch, 0) + 1
+
+        for ch in t:
+            if count.get(ch, 0) == 0:
+                return False
+            count[ch] -= 1
+
+        return True
