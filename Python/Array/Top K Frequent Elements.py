97f6d8205f52601b95c2bf885566c3cfd4ab87aa
williamsk3vin <williamsk3vin@gmail.com>
2026-07-25 19:00:52 -0700

    Update Top K Frequent Elements.py

--- Python/Array/Top K Frequent Elements.py
+++ Python/Array/Top K Frequent Elements.py
@@ -7,3 +7,20 @@ class Solution:
         result = sorted(freq, key=lambda item: item[1], reverse=True)
         first_elements = [item[0] for item in result[:k]]
         return first_elements
+
+
+# Bucket Sort
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        counter = {}
+        answer = []
+        for num in nums:
+            counter[num] = counter.get(num,0) + 1
+        buckets = [[] for _ in range(len(nums) + 1)]
+        for number, frequency in counter.items():
+              buckets[frequency].append(number)
+        for frequency in range(len(buckets) - 1, 0, -1):
+            for number in buckets[frequency]:
+                answer.append(number)
+                if len(answer) == k:
+                    return answer
