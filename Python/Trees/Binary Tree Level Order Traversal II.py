061a81ab39f55ac7f0e412bbbd3a87153bb6f712
williamsk3vin <williamsk3vin@gmail.com>
2026-08-06 07:41:36 -0700

    Create Binary Tree Level Order Traversal II.py

---
+++ Python/Trees/Binary Tree Level Order Traversal II.py
@@ -0,0 +1,56 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+        result = []
+        queue = [root]
+
+        while queue:
+            level_size = len(queue)
+            level = []
+
+            for _ in range(level_size):
+                node = queue.pop(0)
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.insert(0,level)
+        return result
+
+
+# Second attempt, runs much faster
+# created normal list and returned the reverse
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+        result = []
+        queue = [root]
+
+        while queue:
+            level_size = len(queue)
+            level = []
+
+            for _ in range(level_size):
+                node = queue.pop(0)
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+        return result[::-1]
